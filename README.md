80b59f8d6c10d2bd9e2c3c186cdc2344ab56669c
Robert Honsby <rhonsby@gmail.com>
2021-06-07 16:19:40 -0400

    Adds plugin config

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# docusaurus
+# @papercups-io/docusaurus
+
+This plugins enable usage of the Papercups chat widget on Docusaurus powered websites.
+
+## Install
+
+```
+npm install --save @papercups-io/docusaurus
+```
+
+## Usage
+
+1. Sign up at https://app.papercups.io/register to get your account token. Your account token is what you will use to pass in as the accountId prop below.
+
+2. Configure the plugin in `docusaurus.config.js`:
+
+```
+// docusaurus.config.js
+module.exports = {
+  plugins: [
+    [
+      "@papercups-io/docusaurus-plugin",
+      {
+        /* REQUIRED */
+        // Pass in your Papercups account token here after signing up
+        accountId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx',
+
+        /* OPTIONAL */
+        title: 'Welcome to Papercups!',
+        subtitle: 'Ask us anything in the chat window below 😊',
+        newMessagePlaceholder: 'Start typing...',
+        primaryColor: '#13c2c2',
+
+        // Initial message that new users will see.
+        greeting: 'Hi there! How can I help you?',
+
+        // Used only when self-hosting Papercups. Otherwise leave it empty.
+        baseUrl: 'https://app.papercups.io',
+
+        // Add this if you want to require the customer to enter
+        // their email before being able to send you a message
+        requireEmailUpfront: true,
+
+        // Add this if you want to indicate when you/your agents
+        // are online or offline to your customers
+        showAgentAvailability: true,
+      },
+    ],
+  ],
+};
+```
---
+++ src/index.js
@@ -0,0 +1,74 @@
+module.exports = function (_context, options) {
+  const {
+    accountId,
+    baseUrl = "https://app.papercups.io",
+    greeting,
+    newMessagePlaceholder,
+    primaryColor = "#1890ff",
+    requireEmailUpfront,
+    showAgentAvailability,
+    subtitle,
+    title,
+  } = options;
+
+  // Removes the non-nullish values to condense the injected html.
+  const config = Object.entries({
+    accountId,
+    baseUrl,
+    greeting,
+    newMessagePlaceholder,
+    primaryColor,
+    requireEmailUpfront,
+    showAgentAvailability,
+    subtitle,
+    title,
+  })
+    .filter((_key, value) => value != null)
+    .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+  if (!accountId) {
+    throw new Error(`'accountId' missing in plugin options.`);
+  }
+
+  return {
+    name: "@papercups-io/docusaurus",
+    injectHtmlTags() {
+      return {
+        postBodyTags: [
+          {
+            tagName: "script",
+            attributes: {
+              type: "text/javascript",
+            },
+            innerHTML: `window.Papercups={config:${JSON.stringify(config)}};`,
+          },
+          // When the window's width is 996px and below, docusaurus has a sidebar button.
+          // These styles moves the chat toggle button above it to prevent the two buttons from overlapping.
+          {
+            tagName: "style",
+            innerHTML: `
+              @media (max-width: 996px) {
+                .Papercups-toggleButtonContainer {
+                  bottom: 80px;
+                }
+
+                .Papercups-chatWindowContainer {
+                  bottom: 160px;
+                }
+              }
+            `,
+          },
+          {
+            tagName: "script",
+            attributes: {
+              type: "text/javascript",
+              async: true,
+              defer: true,
+              src: `${baseUrl}/widget.js`,
+            },
+          },
+        ],
+      };
+    },
+  };
+};
